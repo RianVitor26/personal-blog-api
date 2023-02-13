ebe4825f16c150b36787360c03bce9650f8ef7a1
Rian Vitor de Almeida <77061521+RianVitor26@users.noreply.github.com>
2023-02-12 21:08:48 -0300

    test: teste da nova action de pr (#2)
    Co-authored-by: RianVitor26 <rianvitorlhe@gamil.com>

---
+++ src/tests/initial.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test, describe } from 'vitest'
+
+describe('test pull request action', () => {
+    test('Should pull request have a information about new action', () => {
+        expect(true).toBe(true)
+    })
+})
