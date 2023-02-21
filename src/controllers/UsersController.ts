12101cd9a333ce68b3654b683dc750da41093d46
RianVitor26 <rianvitorlhe@gamil.com>
2023-02-21 20:14:44 -0300

    feat: adicionando metodo showOne do controller de users

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -14,7 +14,13 @@ class UsersController {
 
   public async showOne(req: Request, res: Response) {
     try {
-      res.status(200).json({ message: 'Show one user' });
+      const { id } = req.params;
+      const user = await UserModel.findById(id)
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+       return res.status(200).json(user)
+      
     } catch (err) {
       res.status(500).json({ message: 'internal server error' });
       console.log(err);
@@ -22,10 +28,9 @@ class UsersController {
   }
 
   public async create(req: Request, res: Response) {
-    
     try {
       const { name, email, password } = req.body;
-      const userEmail = await UserModel.findOne({email});
+      const userEmail = await UserModel.findOne({ email });
       if (userEmail) {
         return res.status(402).json({ message: 'Email already exists' });
       }
@@ -36,9 +41,10 @@ class UsersController {
       console.log(err);
     }
   }
-  public update(req: Request, res: Response) {
+
+  public async update(req: Request, res: Response) {
     try {
-      res.status(200).json({ message: 'User updated successfully' });
+     
     } catch (err) {
       res.status(500).json({ message: 'internal server error' });
       console.log(err);
@@ -47,7 +53,7 @@ class UsersController {
 
   public destroy(req: Request, res: Response) {
     try {
-      res.status(200).json({ message: 'User destroyed successfully' });
+     
     } catch (err) {
       res.status(500).json({ message: 'internal server error' });
       console.log(err);
