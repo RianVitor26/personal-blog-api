c7952a8fba9a7142daba4bc20bd42d5317466fe0
RianVitor26 <rianvitorlhe@gamil.com>
2023-02-28 05:59:34 -0300

    feat: adicionando rotas de posts

--- src/routes/PostsRoute.ts
+++ src/routes/PostsRoute.ts
@@ -3,4 +3,10 @@ import { postsController } from './../controllers/PostsController';
 
 const PostsRoute: Router = Router();
 
+PostsRoute.get('/users/:user_id/posts', postsController.getAll)
+PostsRoute.get('/users/:user_id/posts/:post_id', postsController.getById)
+PostsRoute.post('/users/:user_id/posts', postsController.create)
+PostsRoute.put('/users/:user_id/posts/:post_id', postsController.update);
+PostsRoute.delete('/users/:user_id/posts/:post_id', postsController.remove);
+
 export { PostsRoute };
