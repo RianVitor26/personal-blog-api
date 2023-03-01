b5ffd726c77374c43fbcd51e5103fb860c3cda1a
RianVitor26 <rianvitorlhe@gamil.com>
2023-03-01 15:45:36 -0300

    feat: adicionando método get para as categorias

--- src/controllers/CategoriesController.ts
+++ src/controllers/CategoriesController.ts
@@ -1,15 +1,36 @@
 import { Request, Response } from 'express';
 import { PostModel } from './../models/PostsModel';
 import { CommentModel } from '../models/CommentsModel';
+import { CategoriesModel } from './../models/CategoriesModel';
 
 
 class CategoriesController {
   public async showAll(req: Request, res: Response) {
-   
+    try {
+      const categories = await CategoriesModel.find()
+      if (!categories) return res.status(404).json() 
+    } catch (error) {
+      res.status(500).json({ message: 'internal server error' });
+      console.log(error);
+    }
   }
 
   public async showOne(req: Request, res: Response) {
-    
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(404).json()
+      }
+      const category = await CategoriesModel.findById(id)
+      if (!category) {
+        return res.status(404).json()
+      } 
+      return res.status(200).json()
+      
+    } catch (error) {
+      res.status(500).json({ message: 'internal server error' });
+      console.log(error);
+    }
   }
 
   public async create(req: Request, res: Response) {
